3fb12e5a9048415dd4af42d3b7e08de9d18c3352
tizkovatereza <135881365+tizkovatereza@users.noreply.github.com>
2023-08-12 14:59:05 +0200

    Update ControlFlow_Projects.py

--- ControlFlow_Projects.py
+++ ControlFlow_Projects.py
@@ -1,1 +1,22 @@
+name = "Unicorn"
+question = "Are unicorns real?"
 
+#Yes - definitely
+#It is decidedly so
+#Without a doubt
+#Reply hazy, try again
+#Ask again later
+#Better not tell you now
+#My sources say no
+#Outlook not so good
+#Very doubtful
+
+
+
+
+
+#[Name] asks: [Question]
+#Magic 8-Ball’s answer: [Answer]
+
+#Joe asks: Is this real life?
+#Magic 8-Ball's answer: Better not tell you now
