7b3707d069f6ad71d7b32a25da6170a8bc42f6b9
tizkovatereza <135881365+tizkovatereza@users.noreply.github.com>
2023-08-13 16:30:26 +0200

    Update Lists_Projects.py

--- Lists_Projects.py
+++ Lists_Projects.py
@@ -31,3 +31,69 @@ print(full_gradebook)
 print(subjects)
 print(grades)
 print(gradebook)
+
+
+################################################################################################################################################
+
+# Your code below:
+
+toppings = ["pepperoni",
+"pineapple",
+"cheese",
+"sausage",
+"olives",
+"anchovies",
+"mushrooms"]
+
+prices = [2,
+6,
+1,
+3,
+2,
+7,
+2]
+
+num_two_dollar_slices = prices.count(2)
+num_pizzas = len(toppings)
+
+print("Number of occurences of 2 in the prices is: " + str(num_two_dollar_slices))
+print("Length of toppings list is: " + str(num_pizzas))
+
+print("We sell " + str(num_pizzas) + " different kinds of pizza!")
+
+pizza_and_prices = [[2,"pepperoni"],[6,"pineapple"],[1,"cheese"],[3,	"sausage"],[2,	"olives"],[7,"anchovies"],[	2, "mushrooms"]]
+
+pizza_and_prices.sort()
+cheapest_pizza = pizza_and_prices[0]
+priciest_pizza = pizza_and_prices[-1]
+
+pizza_and_prices.pop(-2)
+pizza_and_prices.apend[2.5, "peppers"]
+
+
+print(pizza_and_prices)
+print("The cheapest pizza is: " + str(cheapest_pizza))
+print("The priciest pizza is: " + str(priciest_pizza))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
