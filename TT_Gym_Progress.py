99656c2e56961789be9a03e21feeba68a56fda3c
tizkovatereza <135881365+tizkovatereza@users.noreply.github.com>
2023-08-17 23:43:28 +0200

    Create TT_Gym_Progress.py

---
+++ TT_Gym_Progress.py
@@ -0,0 +1,11 @@
+gym_progress = {"Low row": "20 kg",
+                "Lat machine convergent": "30 kg",
+               "Rear delt fly": "5 kg each side",
+               "Overhead triceps extensions": "10 kg",
+               "Lat pulldown": "35 kg",
+               "Inclined chest press": "10 kg",
+               }
+
+
+print(gym_progress["Low row"])
+my_dictionary["Low row"] = "20 kg"
