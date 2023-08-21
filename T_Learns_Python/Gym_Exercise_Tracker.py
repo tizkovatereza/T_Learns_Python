7e60568a6ddce78d04e744e70de26155cc1a2131
tizkovatereza <135881365+tizkovatereza@users.noreply.github.com>
2023-08-21 18:14:36 +0200

    Update Gym_Exercise_Tracker.py

--- T_Learns_Python/Gym_Exercise_Tracker.py
+++ T_Learns_Python/Gym_Exercise_Tracker.py
@@ -2,12 +2,20 @@
 
 # Dictionary to store gym exercises and their weights
 gym_exercises = {
-    "Low row": "20 kg",
-    "Lat machine convergent": "30 kg",
-    "Rear delt fly": "5 kg each side",
-    "Overhead triceps extensions": "10 kg",
-    "Lat pulldown": "35 kg",
-    "Inclined chest press": "10 kg",
+"Low row": "20 kg",
+"Lat machine convergent": "30 kg",
+"Rear delt fly": "4 kg each side",
+"Overhead triceps extensions": "10 kg",
+"Lat pulldown": "35 kg",
+"Inclined chest press": "10 kg",
+"squat": "50 kg",
+"hip thrust": "100 kg",
+"master gluteus": "40 kg",
+"abductor": "60 kg",
+"adductor": "65 kg",
+"torsion": "35 kg",
+"deltoid press": "10 kg"
+    
 }
 
 def display_exercises():
