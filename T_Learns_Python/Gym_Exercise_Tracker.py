f329ff1a67bc0ebc576a569256b7f8a3e43d11dc
tizkovatereza <135881365+tizkovatereza@users.noreply.github.com>
2023-08-21 16:02:35 +0200

    Update Gym_Exercise_Tracker.py

--- T_Learns_Python/Gym_Exercise_Tracker.py
+++ T_Learns_Python/Gym_Exercise_Tracker.py
@@ -19,24 +19,14 @@ def update_exercise(exercise, new_weight):
     """Updates the weight of an exercise."""
     gym_exercises[exercise] = new_weight
 
-def add_exercise(exercise, weight):
-    """Adds a new exercise and its weight."""
-    gym_exercises[exercise] = weight
-
 # Display the current list of exercises and their weights
 display_exercises()
 
-# Allow the user to update the weight of any exercise or add a new one
+# Allow the user to update the weight of any exercise
 while True:
-sweep/add-new-exercise-option
-    exercise = input("Enter the name of the exercise to update or add (or 'quit' to exit): ")
+    exercise = input("Enter the name of the exercise to update (or 'quit' to exit): ")
     if exercise.lower() == 'quit':
         break
     new_weight = input("Enter the new weight for the exercise: ")
-    if exercise not in gym_exercises:
-        print(f"Adding new exercise {exercise} with weight {new_weight}")
-    else:
-        print(f"Updating exercise {exercise} to new weight {new_weight}")
-
-        main
-
+    update_exercise(exercise, new_weight)
+    print
