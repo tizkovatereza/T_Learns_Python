2ca605e97ff0aba4f3190ed0d6c689dc6ebfb9fd
Tereza Tizkova <tereza@e2b.dev>
2024-01-09 16:40:08 +0100

    update

--- Convert-MD-to-CSV/convert-md-to-csv.py
+++ Convert-MD-to-CSV/convert-md-to-csv.py
@@ -15,27 +15,27 @@ def create_empty_csv(file_path, columns):
    df.to_csv(file_path, index=False)
 
 def populate_csv(html_path, csv_path):
-  # Parse the HTML file
-  with open(html_path, 'r') as f:
-      contents = f.read()
+ # Parse the HTML file
+ with open(html_path, 'r') as f:
+     contents = f.read()
 
-  soup = BeautifulSoup(contents, 'lxml')
+ soup = BeautifulSoup(contents, 'lxml')
 
-  # Initialize name and category
-  name = ""
-  category = ""
+ # Initialize name and category
+ name = ""
+ category = ""
 
-  # Iterate over the h1 and h2 tags
-  for tag in soup.find_all(['h1', 'h2']):
-      # Update the category if the tag is a h1 tag
-      if tag.name == 'h1':
-          category = 'Open source' if 'open_hands: Open-source projects' in str(tag) else 'Closed source'
-      # Update the name if the tag is a h2 tag and the category is not empty
-      elif tag.name == 'h2' and category:
-          name = tag.text
-          # Append the name and category to the CSV file
-          df = pd.DataFrame({'Name': [name], 'Category': [category]})
-          df.to_csv(csv_path, mode='a', header=False, index=False)
+ # Iterate over the h1 and h2 tags
+ for tag in soup.find_all(['h1', 'h2']):
+     # Update the category if the tag is a h1 tag
+     if tag.name == 'h1':
+         category = 'Open source' if 'open_hands: Open-source projects' in str(tag) else 'Closed source'
+     # Update the name if the tag is a h2 tag and the category is not empty
+     elif tag.name == 'h2' and category:
+         name = tag.text
+         # Append the name and category to the CSV file
+         df = pd.DataFrame({'Category': [category], 'Name': [name]})
+         df.to_csv(csv_path, mode='a', header=False, index=False)
 
 
 if __name__ == "__main__":
