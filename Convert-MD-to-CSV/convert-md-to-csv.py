3d46b71251ff443a96b9ff1bb02dcdbd39b46442
Tereza Tizkova <tereza@e2b.dev>
2024-01-10 15:03:15 +0100

    Update

--- Convert-MD-to-CSV/convert-md-to-csv.py
+++ Convert-MD-to-CSV/convert-md-to-csv.py
@@ -15,28 +15,39 @@ def create_empty_csv(file_path, columns):
    df.to_csv(file_path, index=False)
 
 def populate_csv(html_path, csv_path):
- # Parse the HTML file
- with open(html_path, 'r') as f:
-     contents = f.read()
+   # Parse the HTML file
+   with open(html_path, 'r') as f:
+       contents = f.read()
 
- soup = BeautifulSoup(contents, 'lxml')
+   soup = BeautifulSoup(contents, 'lxml')
 
- # Initialize name and category
- name = ""
- category = ""
+   # Initialize name, category, tagline, and description
+   name = ""
+   category = ""
+   tagline = ""
+   description = ""
 
- # Iterate over the h1 and h2 tags
- for tag in soup.find_all(['h1', 'h2']):
-     # Update the category if the tag is a h1 tag
-     if tag.name == 'h1':
-         category = 'Open source' if 'open_hands: Open-source projects' in str(tag) else 'Closed source'
-     # Update the name if the tag is a h2 tag and the category is not empty
-     elif tag.name == 'h2' and category:
-         name = tag.text
-         # Append the name and category to the CSV file
-         df = pd.DataFrame({'Category': [category], 'Name': [name]})
-         df.to_csv(csv_path, mode='a', header=False, index=False)
-
+   # Iterate over the h1, h2, and h3 tags
+   for tag in soup.find_all(['h1', 'h2', 'h3']):
+       # Update the category if the tag is a h1 tag
+       if tag.name == 'h1':
+           category = 'Open source' if 'open_hands: Open-source projects' in str(tag) else 'Closed source'
+       # Update the name if the tag is a h2 tag and the category is not empty
+       elif tag.name == 'h2' and category:
+           name = tag.text
+           # Find the next p tag and update the tagline
+           next_p = tag.find_next('p')
+           if next_p:
+               tagline = next_p.text.replace(',', ' ') # Replace commas with spaces
+       # Update the description if the tag is a h3 tag and its text is 'Description'
+       elif tag.name == 'h3' and tag.text.strip() == 'Description':
+           # Find the next details tag and update the description
+           next_details = tag.find_next('details')
+           if next_details:
+               description = str(next_details).strip().replace(',', ' ') # Convert the details tag to a string
+           # Append the name, category, tagline, and description to the CSV file
+           df = pd.DataFrame({'Category': [category], 'Name': [name], 'Tagline': [tagline], 'Description': [description]})
+           df.to_csv(csv_path, mode='a', header=False, index=False)
 
 if __name__ == "__main__":
    md_file_path = "README.md" # replace with your file path
