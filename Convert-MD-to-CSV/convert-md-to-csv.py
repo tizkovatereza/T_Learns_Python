e8c3ab02a8ea16c5ae43242b514c8afafcf4a05b
Tereza Tizkova <tereza@e2b.dev>
2024-01-11 13:27:08 +0100

    update algorithm

--- Convert-MD-to-CSV/convert-md-to-csv.py
+++ Convert-MD-to-CSV/convert-md-to-csv.py
@@ -9,78 +9,68 @@ def convert_md_to_html(file_path):
    html = marko.convert(text)
    return html
 
-def create_empty_csv(file_path, columns):
-   # Create an empty DataFrame with specified columns
-   df = pd.DataFrame(columns=columns)
-   # Save the DataFrame to a CSV file
-   df.to_csv(file_path, index=False)
-
 def populate_csv(html_path, csv_path):
- # Parse the HTML file
- with open(html_path, 'r') as f:
-     contents = f.read()
+    # Parse the HTML file
+    with open(html_path, 'r') as f:
+        contents = f.read()
 
- soup = BeautifulSoup(contents, 'lxml')
+    soup = BeautifulSoup(contents, 'lxml')
 
- # Initialize name, category, tagline, description, use_cases, and image_url
- name = ""
- category = ""
- tagline = ""
- description = ""
- use_cases = ""
- image_url = ""
+    rows = []
+    details = soup.find_all('details')
+    for detail in details:  
+        oss_category = detail.find_previous('h1').text
+        agent_name = detail.find_previous('h2').text
+        tagline = detail.find_previous('p').text
+        
+        # Dict that represents a single row
+        agent_row = { 'Category': oss_category, 'Name': agent_name, 'Tagline': tagline}
 
- # Iterate over the h1, h2, and h3 tags
- for tag in soup.find_all(['h1', 'h2', 'h3']):
-     # Update the category if the tag is a h1 tag
-     if tag.name == 'h1':
-         category = 'Open source' if 'open_hands: Open-source projects' in str(tag) else 'Closed source'
-     # Update the name if the tag is a h2 tag and the category is not empty
-     elif tag.name == 'h2' and category:
-         name = tag.text
-         # Find the next p tag and update the tagline
-         next_p = tag.find_next('p')
-         if next_p:
-             tagline = next_p.text
-     # Update the use_cases if the tag is a h3 tag and its text is 'Category'
-     elif tag.name == 'h3' and tag.text.strip() == 'Category':
-         # Find the next p tag and update the use_cases
-         next_p = tag.find_next('p')
-         if next_p:
-             use_cases = next_p.text.strip() # Extract the text from the next p tag
-     # Update the description if the tag is a h3 tag and its text is 'Description'
-     elif tag.name == 'h3' and tag.text.strip() == 'Description':
-         # Find the next details tag and update the description
-         next_details = tag.find_next('details')
-         if next_details:
-             description = str(next_details).strip() # Convert the details tag to a string
-     # Update the image_url if the tag is a h3 tag and its text is 'Category'
-     elif tag.name == 'h3' and tag.text.strip() == 'Category':
-         # Find the previous details tag and update the image_url
-         prev_details = tag.find_previous('details')
-         if prev_details:
-             image_match = re.search(r'!\[(.*?)\]\((.*?)\)', str(prev_details))
-             if image_match:
-                image_url = image_match.group(2) # Extract the URL from the match
-     # Append the name, category, tagline, description, use_cases, and image_url to the CSV file
-     df = pd.DataFrame({'Category': [category], 'Name': [name], 'Tagline': [tagline], 'Description': [description], 'Use-cases': [use_cases], 'Image': [image_url]})
-     df.to_csv(csv_path, mode='a', header=False, index=False)
+        logo_url = detail.find('img')
+        if logo_url is not None:
+            # logo_url = logo_url['src']            
+            agent_row["Image"] = logo_url
+        else:
+            agent_row["Image"] = ""
 
+        agent_use_case_category = detail.find('h3', string='Category')
+        if agent_use_case_category is not None:
+            agent_use_case_category_paragrahp = agent_use_case_category.find_next('p')
+            agent_row["Use-cases"] = agent_use_case_category_paragrahp.text
+        else:
+            agent_row["Use-cases"] = ""
+
+        description_el = detail.find('h3', string='Description')
+        if description_el is not None:                        
+            description = description_el.find_next('ul')
+            agent_row["Description"] = description
+        else:
+            agent_row["Description"] = ""           
+
+        # TODO: Improve parsing links
+        links_el = detail.find('h3', string='Links')
+        if links_el is not None:           
+            links = links_el.find_next('ul')
+            agent_row["Links"] = links
+        else:
+            agent_row["Links"] = ""
+        
+        rows.append(agent_row)
+    
+    df = pd.DataFrame.from_dict(rows, orient='columns')              
+    df.to_csv(csv_path, header=True, index=False)    
+    
 
 if __name__ == "__main__":
- md_file_path = "/Users/terezatizkova/Developer/awesome-ai-agents/README.md" # replace with your file path
- html = convert_md_to_html(md_file_path)
+    md_file_path = "/Users/terezatizkova/Developer/awesome-ai-agents/README.md" # replace with your file path
+    html = convert_md_to_html(md_file_path)
 
- # Save HTML output
- with open("output.html", 'w') as f:
-     f.write(html)
+    # Save HTML output
+    with open("output.html", 'w') as f:
+        f.write(html)
 
- # CSV file path
- csv_file_path = "converted_output.csv"
- # Columns for the CSV file
- columns = ["Category", "Name", "Tagline", "Description", "Use-cases", "Image"]
- # Create and save the empty CSV file
- create_empty_csv(csv_file_path, columns)
+    # CSV file path
+    csv_file_path = "converted_output.csv"
 
- # Populate the CSV file
- populate_csv("output.html", csv_file_path)
+    # Populate the CSV file
+    populate_csv("output.html", csv_file_path)
